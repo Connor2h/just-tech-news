4553e92da0af92a20db870cc56c1e2baec760a23
Connor Hollis <connorhollis3d@gmail.com>
2022-01-11 21:25:35 -0700

    added ability to hash password on post and put routes

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -40,13 +41,35 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-            // this means the password must be at least four characters long
-            len: [4]
+                len: [4]// this means the password must be at least four characters long
             }
         }
     },
     {
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+        ////Different async version of doing the hook below
+        // hooks: {
+        //     // set up beforeCreate lifecycle "hook" functionality
+        //     beforeCreate(userData) {
+        //       return bcrypt.hash(userData.password, 10).then(newUserData => {
+        //         return newUserData
+        //       });
+        //     }
+        //   }
+
+        hooks:{
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
 
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
